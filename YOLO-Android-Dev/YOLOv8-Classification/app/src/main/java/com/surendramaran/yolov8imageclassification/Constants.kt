e74c531c0e3a00c83fb71afa53aa33635e7dae7c
AIsoltani <amirsoltani_os@hotmail.com>
2025-03-21 12:28:40 -0700

    Delete YOLO-Android-Dev/YOLOv8-Classification directory

--- YOLO-Android-Dev/YOLOv8-Classification/app/src/main/java/com/surendramaran/yolov8imageclassification/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.surendramaran.yolov8imageclassification
-
-object Constants {
-    const val MODEL_PATH = "yolov8n-cls_int8.tflite"
-    val LABELS_PATH: String? = null // provide your labels.txt file if the metadata not present in the model
-}
--- YOLO-Android-Dev/YOLOv8-Classification/app/src/main/java/com/surendramaran/yolov8imageclassification/Prediction.kt
+++
@@ -1,7 +0,0 @@
-package com.surendramaran.yolov8imageclassification
-
-data class Prediction(
-    val id: Int,
-    val name: String,
-    val score: Float
-)
